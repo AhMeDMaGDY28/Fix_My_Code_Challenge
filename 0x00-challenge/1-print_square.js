9c78f770c6db2f2dac3725f01a8dd9cef6e32c8d
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-04 06:04:17 +0200

    T1 fix 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 /*
     Print a square with the character #
     
@@ -6,21 +7,22 @@
     of the program.
 */
 
-
-if (process.argv.length <= 2) {
-	process.stderr.write("Missing argument\n");
+if (process.argv.length !== 3) {
+	process.stderr.write("Missing or incorrect argument\n");
 	process.stderr.write("Usage: ./1-print_square.js <size>\n");
 	process.stderr.write("Example: ./1-print_square.js 8\n");
-	process.exit(1)
- }
- 
- size = parseInt(process.argv[2], 0)
- if (isNaN(size) || size <= 0) {
 	process.exit(1);
  }
  
- for (let i = 0 ; i < size ; i ++) {
-	for (let j = 0 ; j < size ; j ++) {
+ const size = parseInt(process.argv[2], 10);
+ 
+ if (isNaN(size) || size <= 0) {
+	process.stderr.write("Invalid size argument\n");
+	process.exit(1);
+ }
+ 
+ for (let i = 0; i < size; i++) {
+	for (let j = 0; j < size; j++) {
 	    process.stdout.write("#");
 	}
 	process.stdout.write("\n");
